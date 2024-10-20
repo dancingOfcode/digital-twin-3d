477c679a6240f003abaa5abfa326596808cd4ec3
zhengyou007@163.com <zhengyou007@163.com>
2024-10-20 23:38:32 +0800

    feat:初始化项目基本框架结构

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="wrapper">
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
